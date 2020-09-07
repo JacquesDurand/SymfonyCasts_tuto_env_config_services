293d4e56b6ca163e5ee08c9ee9afc631ad47d405
JacquesDurand <hugo.nicolas.hn@gmail.com>
2020-09-07 14:49:05 +0200

    Named Autowired log services

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -5,23 +5,34 @@ namespace App\Service;
 
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 
 class MarkdownHelper
 {
     private $markdownParser;
     private $cache;
+    private $isDebug;
+    private $logger;
 
-    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache)
+    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug, LoggerInterface $markdownLogger)
     {
         $this->markdownParser = $markdownParser;
         $this->cache = $cache;
-
+        $this->isDebug = $isDebug;
+        $this->logger = $markdownLogger;
     }
 
-    public function parse (string $source) : string {
-            dump($this->cache);
-      return $parsedString = $this->cache->get('markdown_' . md5($source), function () use ($source) {
+    public function parse(string $source): string
+    {
+        if (stripos($source, 'cat') !== false ) {
+            $this->logger->info('Meow');
+        }
+
+        if ($this->isDebug) {
+            return $this->markdownParser->transformMarkdown($source);
+        }
+        return $parsedString = $this->cache->get('markdown_' . md5($source), function () use ($source) {
             return $this->markdownParser->transformMarkdown($source);
         });
     }
