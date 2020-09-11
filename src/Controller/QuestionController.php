2af2a4fb82ced3c691a61f2e7bdd2631c5cc5ba1
JacquesDurand <hugo.nicolas.hn@gmail.com>
2020-09-11 09:15:29 +0200

    Add voting system

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -7,6 +7,7 @@ use App\Repository\QuestionRepository;
 use App\Service\MarkdownHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -58,6 +59,8 @@ EOF
             $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(0, 100))));
         }
 
+        $question->setVotes(rand(-20,50));
+
         $entityManager->persist($question);
         $entityManager->flush();
 
@@ -70,12 +73,11 @@ EOF
 
     /**
      * @Route("/questions/{slug}", name="app_question_show")
-     * @param $slug
      * @return Response
      */
     public function show(Question $question)
     {
-        
+
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
@@ -86,5 +88,28 @@ EOF
             'question' => $question,
             'answers' => $answers
         ]);
+    }
+
+    /**
+     * @Route("/questions/{slug}/vote", name="app_question_vote", methods="POST")
+     */
+    public function questionVote(Question $question, Request $request, EntityManagerInterface $entityManager) {
+
+        $direction = $request->request->get('direction');
+
+        if ($direction === 'up') {
+            $question->upVote();
+        }
+        elseif ($direction ==='down')
+        {
+            $question->downVote();
+        }
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute("app_question_show",[
+            'slug' => $question->getSlug()
+        ]);
+
     }
 }
